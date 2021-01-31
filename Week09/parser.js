841cdd8e8552f66dc9c3fd3f28f062eb41ad6e38
pengzhiming <pengzhiming@wps.cn>
2021-01-31 22:40:20 +0800

    add css

--- Week09/parser.js
+++ Week09/parser.js
@@ -1,6 +1,10 @@
+const css = require('css');
+
 let currentToken = null;
 let currentAttribute = null;
 let currentTextNode = null;
+
+let rules = [];
 
 let stack = [{ type: "document", children: [] }];
 
@@ -58,6 +62,8 @@ function emit(token) {
             }
         }
 
+        computeCSS(element);
+
         top.children.push(element);
         element.parent = top;
 
@@ -70,6 +76,9 @@ function emit(token) {
         if (top.tagName !== token.tagName) {
             throw new Error("Tag start end doesn't match!");
         } else {
+            if (top.tagName === "style") {
+                addCSSRules(top.children[0].content);
+            }
             stack.pop();
         }
         currentTextNode = null;
@@ -83,6 +92,117 @@ function emit(token) {
         }
         currentTextNode.content += token.content;
     }
+}
+
+function addCSSRules(text) {
+    var ast = css.parse(text);
+    rules.push(...ast.stylesheet.rules);
+}
+
+function computeCSS(element) {
+    var elements = stack.slice().reverse();
+    if (!element.computedStyle) {
+        element.computedStyle = {};
+    }
+
+    for (let rule of rules) {
+        var selectorParts = rule.selectors[0].split(" ").reverse();
+
+        console.log(selectorParts);
+        console.log(match(element, selectorParts[0]));
+
+        if (!match(element, selectorParts[0])) {
+            continue;
+        }
+
+        let matched = false;
+
+        var j = 1;  // current selector position
+        for (var i = 0; i < elements.length; i++) { // for each element
+            if (match(elements[i], selectorParts[j])) {
+                j++;
+            }
+        }
+
+        if (j >= selectorParts.length) {
+            matched = true;
+        }
+
+        console.log(matched);
+
+        if (matched) {
+            console.log("Element", element, "matched rule", rule);
+            var sp = specificity(rule.selectors[0]);
+            var computedStyle = element.computedStyle;
+            for (var declaration of rule.declarations) {
+                if (!computedStyle[declaration.property]) {
+                    computedStyle[declaration.property] = {};
+                }
+
+                if (!computedStyle[declaration.property].specificity) {
+                    computedStyle[declaration.property].value = declaration.value;
+                    computedStyle[declaration.property].specificity = sp;
+                } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
+                    computedStyle[declaration.property].value = declaration.value;
+                    computedStyle[declaration.property].specificity = sp;
+                }
+            }
+            console.log(element.computedStyle);
+        }
+    }
+}
+
+// Assuming all selectors are simple selectors
+function match(element, selector) {
+    console.log("match", element.tagName, selector);
+    if (!selector || !element.attributes) { // use .attributes to determine if the current elemenet is a text node
+        return false;
+    }
+
+    if (selector.charAt(0) === "#") {
+        var attr = element.attributes.filter(attr => attr.name === "id")[0];
+        if (attr && attr.value === selector.replace("#", "")) {
+            return true;
+        }
+    } else if (selector.charAt(0) === ".") {
+        var attr = element.attributes.filter(attr => attr.name === "class")[0];
+        if (attr && attr.value === selector.replace(".", "")) {
+            return true;
+        }
+    } else {
+        if (element.tagName === selector) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function specificity(selector) {
+    var p = [0, 0, 0, 0];
+    var selectorParts = selector.split(" ");
+    for (var part of selectorParts) {
+        if (part.charAt(0) === "#") {
+            p[1] += 1;
+        } else if (part.charAt(0) === ".") {
+            p[2] += 1;
+        } else {
+            p[3] += 1;
+        }
+    }
+    return p;
+}
+
+function compare(sp1, sp2) {
+    if (sp1[0] - sp2[0]) {
+        return sp1[0] - sp2[0];
+    }
+    if (sp1[1] - sp2[1]) {
+        return sp1[1] - sp2[1];
+    }
+    if (sp1[2] - sp2[2]) {
+        return sp1[2] - sp2[2];
+    }
+    return sp1[3] - sp2[3];
 }
 
 function tagOpen(c) {
@@ -180,6 +300,7 @@ function afterAttributeName(c) {
         if (currentAttribute && currentAttribute.name) {
             currentToken[currentAttribute.name] = currentAttribute.value;
         }
+
         currentAttribute = {
             name: "",
             value: ""
